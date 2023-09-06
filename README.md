5e2c620480982e6d1b7d1d0725bb23fc4a76f329
Shakhmurad <muradb356@gmail.com>
2023-09-06 14:19:59 +0400

    add swiper

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # React + Vite
-
+Figma - https://www.figma.com/file/Ll2WWiOKcLQildT6f6hbzt/Xa%C3%A7maz-Agro?type=design&node-id=0-1&mode=design&t=4cptbykdBV0T4B83-0
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,19 +1,49 @@
 import React from 'react'
 import LayoutCompanent from '../../layoutCompanent/layoutCompanent'
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 function Home() {
   return (
     <LayoutCompanent>
-          <div className="header-content">
-    <img src={'./img/Group 169.png'} />
-    <div className="header-menu-icon">
+
+    <Swiper
+      // install Swiper modules
+      modules={[Autoplay,Navigation, Pagination, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      loop={true}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction:false
+    }}
+    >
+      <SwiperSlide><img src={'./img/Rectangle 18 (1).png'} alt="" /></SwiperSlide>
+      <SwiperSlide><img src={'./img/Rectangle 18 (2).png'} alt="" /></SwiperSlide>
+      <SwiperSlide><img src={'./img/Rectangle 12.png'} alt="" /></SwiperSlide>
+      <SwiperSlide><img src={'./img/Rectangle 18.png'} alt="" /></SwiperSlide>
+      <div className="header-content">
+        <img src={'./img/Group 169.png'} />
+      <div className="header-menu-icon">
         <div className="header-lag">
           <p>En</p>
           <p>Ru</p>
         </div>
         <i class="fa-solid fa-bars"></i>
     </div>
-  </div>
+    </div>
+    <div className="swiper-pos"></div>
+    </Swiper>
     </LayoutCompanent>
   )
 }
