84af975674f619f3a51c9d0e0a8b844dad1773ff
Shakhmurad <muradb356@gmail.com>
2023-09-08 15:23:03 +0400

    add navbar

--- src/Pages/AboutPage/About.jsx
+++ src/Pages/AboutPage/About.jsx
@@ -8,10 +8,17 @@ function About() {
   return (
     <LayoutCompanent>
       <NavbarContent/>
-  <div className={style["content"]}>
-
-  </div>
         <NavBar/>
+  <div className={style["content"]}>
+        <div className={style["content-top-text"]}>
+            <p className='pElement-content'>ana səhifə</p>
+            <p className='pElement-bold'>haqqımızda</p>
+        </div>
+      <div className={style['content-text']}>
+          <p>Haqqımızda</p>
+          <h2>Biz aqrobiznes kommunikasiyaları ilə məşğul oluruq</h2>
+      </div>
+  </div>
     </LayoutCompanent>
   )
 }
