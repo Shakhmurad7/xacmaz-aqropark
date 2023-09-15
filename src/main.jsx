d0d91023d3b0166f1ea8a28840039acd1f078273
Shakhmurad <muradb356@gmail.com>
2023-09-15 13:50:59 +0400

    add redax

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
 import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import {createStore} from "redux"
-// import {Provider} from 'react-redux'
+import {createStore} from "redux"
+import {Provider} from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-// import rooteredux from './Pages/Qalereya/redux.jsx'
+import rooteredux from './Pages/Qalereya/redux.jsx'
 
 
-// const store = createStore(rooteredux)
+const store = createStore(rooteredux)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-{/* <Provider store={store}> */}
+<Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-{/* </Provider> */}
+</Provider>
 </>
 )
